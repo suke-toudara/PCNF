e185ee14a3fa46687ea05c9f3bfa970eba00fb57
keisuke yoshida <82552894+suke-toudara@users.noreply.github.com>
2024-10-25 11:30:29 +0900

    Update and rename pass_through_filter.hpp to field_limit_filter.hpp

---
+++ include/cloud_cleaner/field_limit_filter.hpp
@@ -0,0 +1,29 @@
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+
+class PassThroughFilter {
+public:
+    pcl::PointCloud<pcl::PointXYZ>::Ptr apply(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+        pcl::PassThrough<pcl::PointXYZ> pass_filter;
+        //LiDAR area limmit
+     　 // x axis
+        pass_filter.setInputCloud(filtered_cloud); // zでフィルタされた結果を使用
+        pass_filter.setFilterFieldName("x");
+        pass_filter.setFilterLimits(-10.0, 10.0);  // x方向のフィルタ範囲
+        pass_filter.filter(*filtered_cloud);
+
+        // y axis
+        pass_filter.setInputCloud(filtered_cloud); // xでフィルタされた結果を使用
+        pass_filter.setFilterFieldName("y");
+        pass_filter.setFilterLimits(-5.0, 5.0);    // y方向のフィルタ範囲
+        pass_filter.filter(*filtered_cloud);
+        // z axis
+        pass_filter.setInputCloud(cloud);
+        pass_filter.setFilterFieldName("z");  
+        pass_filter.setFilterLimits(0.0, 3.5); 
+        pass_filter.filter(*filtered_cloud);
+        return filtered_cloud;
+    }
+};
